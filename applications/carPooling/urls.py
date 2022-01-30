a5721264b2df04be44eb36948386f6afef695bc5
Valvanera Gijon <valvaneragijonjimenez@gmail.com>
2022-01-30 15:05:37 +0100

    feat: status endpoint created

--- applications/carPooling/urls.py
+++ applications/carPooling/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('status/', views.StatusView.as_view()), 
+]
--- applications/carPooling/views.py
+++ applications/carPooling/views.py
@@ -1,2 +1,14 @@
 from django.shortcuts import render
 
+from rest_framework.generics import views
+from rest_framework.response import Response
+
+from .serializers import StatusSerializer
+
+
+class StatusView(views.APIView):
+
+    def get(self, request):        
+        status = {"status": "Car Pooling Service is working"}
+        results = StatusSerializer(status).data
+        return Response(results)
