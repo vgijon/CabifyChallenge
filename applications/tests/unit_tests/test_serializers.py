b122490bd51243f906acb0fb15fca99925e92d8e
Valvanera Gijon <valvaneragijonjimenez@gmail.com>
2022-02-02 19:33:02 +0100

    test: unit_tests estructure

---
+++ applications/tests/unit_tests/test_serializers.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class SerializerTest(unittest.TestCase):
+    """
+    SerializerTest class to testing Django Serializers
+
+    Args:
+        unittest ([type]): [description]
+    """  
+
+    def BuildTest(self):
+        """
+        Test data builder
+        """
+        # Build all objects for the tests
+
+    def setUp(self):
+        """
+        Function setUp to build objects
+        """
+        self.BuildTest()
+
+    def test_carSerializer(self):
+        pass
+
+    def test_journeySerializer(self):
+        pass
+
+    def test_droppOffSerializer(self):
+        pass
+
+    def test_localeSerializer(self):
+        pass
---
+++ applications/tests/unit_tests/test_views.py
@@ -0,0 +1,34 @@
+import unittest
+
+
+class ViewsTest(unittest.TestCase):
+    """
+    ViewsTest class to testing Django Views
+
+    Args:
+        unittest ([type]): [description]
+    """  
+
+    def BuildTest(self):
+        """
+        Test data builder
+        """
+        # Build all objects for the tests
+
+    def setUp(self):
+        """
+        Function setUp to build objects
+        """
+        self.BuildTest()
+
+    def test_carView(self):
+        pass
+
+    def test_journeyView(self):
+        pass
+
+    def test_droppOffView(self):
+        pass
+
+    def test_localeView(self):
+        pass
